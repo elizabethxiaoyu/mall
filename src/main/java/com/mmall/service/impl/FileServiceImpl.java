5ca974b3adedcfdf78067ed53da16481f6ba6eda
Alice <Alice>
2018-05-10 00:18:24 +0800

    修改了不删除update文件夹下文件

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -41,7 +41,7 @@ public class FileServiceImpl implements IFileService {
             FTPUtil.uploadFile(Lists.newArrayList(targetFile));
             //已经上传到ftp服务器
 
-            targetFile.delete();
+            //targetFile.delete();
 
         } catch (IOException e) {
             logger.error("上传文件异常",e);
